3ccf33fbc636daad7ebb50e06935b12a410e6509
Juan Manuel Triguero Piñero <juantriguero@live.com>
2016-11-15 02:09:18 +0100

    Editar y eliminar libros

--- EntHub/items/urls.py
+++ EntHub/items/urls.py
@@ -4,4 +4,6 @@ from items import views
 urlpatterns = [
 	url(r'^books$', views.BookList.as_view(), name='book_list'),
 	url(r'^books/new$', views.BookCreate.as_view(), name='book_create'),
+	url(r'^books/edit/(?P<pk>\d+)/$', views.BookUpdate.as_view(), name='book_update'),
+	url(r'^books/delete/(?P<pk>\d+)/$', views.BookDelete.as_view(), name='book_delete'),
 ]
--- EntHub/items/views.py
+++ EntHub/items/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from items import models, forms
 
@@ -13,4 +13,15 @@ class BookCreate(CreateView):
 	model = models.Book
 	form_class = forms.BookForm
 	template_name = 'items/book_create.html'
+	success_url = reverse_lazy('items:book_list')
+
+class BookUpdate(UpdateView):
+	model = models.Book
+	form_class = forms.BookForm
+	template_name = 'items/book_update.html'
+	success_url = reverse_lazy('items:book_list')
+
+class BookDelete(DeleteView):
+	model = models.Book
+	template_name = 'items/book_delete.html'
 	success_url = reverse_lazy('items:book_list')

352d81ab79947ffe01555999e28150659b8118dd
Juan Manuel Triguero Piñero <juantriguero@live.com>
2016-11-15 19:09:59 +0100

    Restricciones login_required

--- EntHub/items/urls.py
+++ EntHub/items/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from items import views
 
 urlpatterns = [
-	url(r'^books$', views.BookList.as_view(), name='book_list'),
-	url(r'^books/new$', views.BookCreate.as_view(), name='book_create'),
-	url(r'^books/edit/(?P<pk>\d+)/$', views.BookUpdate.as_view(), name='book_update'),
-	url(r'^books/delete/(?P<pk>\d+)/$', views.BookDelete.as_view(), name='book_delete'),
+	url(r'^books$', login_required(views.BookList.as_view()), name='book_list'),
+	url(r'^books/new$', login_required(views.BookCreate.as_view()), name='book_create'),
+	url(r'^books/edit/(?P<pk>\d+)/', login_required(views.BookUpdate.as_view()), name='book_update'),
+	url(r'^books/delete/(?P<pk>\d+)/', login_required(views.BookDelete.as_view()), name='book_delete'),
 ]
--- EntHub/main/urls.py
+++ EntHub/main/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login, logout_then_login
+from django.contrib.auth.decorators import login_required
 from main import views
-from django.contrib.auth.views import login
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-    url(r'^login$', login, {'template_name': 'main/login.html'}, name='login'),
+	url(r'^$', login_required(views.index), name='index'),
+    url(r'^accounts/login/', login, {'template_name': 'main/login.html'}, name='login'),
+    url(r'^accounts/logout/', logout_then_login, name='logout'),
 ]

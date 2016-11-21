e3e366a1d54b919dd4cde93912d7f1b91d6ce4fc
Juan Manuel Triguero Piñero <juantriguero@live.com>
2016-11-21 18:02:50 +0100

    Vista de ficha de libro

--- EntHub/items/urls.py
+++ EntHub/items/urls.py
@@ -4,6 +4,7 @@ from items import views
 
 urlpatterns = [
 	url(r'^books$', login_required(views.BookList.as_view()), name='book_list'),
+	url(r'^books/(?P<pk>\d+)/', login_required(views.BookDetail.as_view()), name='book_detail'),
 	url(r'^books/new$', login_required(views.BookCreate.as_view()), name='book_create'),
 	url(r'^books/edit/(?P<pk>\d+)/', login_required(views.BookUpdate.as_view()), name='book_update'),
 	url(r'^books/delete/(?P<pk>\d+)/', login_required(views.BookDelete.as_view()), name='book_delete'),
--- EntHub/items/views.py
+++ EntHub/items/views.py
@@ -1,5 +1,6 @@
+import math
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from items import models, forms
 
@@ -8,6 +9,20 @@ from items import models, forms
 class BookList(ListView):
 	model = models.Book
 	template_name = 'items/book_list.html'
+
+class BookDetail(DetailView):
+	model = models.Book
+	template_name = 'items/book_detail.html'
+	
+	def get_context_data(self, **kwargs):
+		context = super(BookDetail, self).get_context_data(**kwargs)
+		# TODO Bidireccionalidad
+		involvements = []
+		for i in models.BookInvolvement.objects.all():
+			if i.book == self.object:
+				involvements.append(i)
+		context['involvements'] = involvements
+		return context
 
 class BookCreate(CreateView):
 	model = models.Book

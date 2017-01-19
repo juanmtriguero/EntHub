6e4f1be85b559c689a09f5b9b7eab918c15cecd4
Juan Manuel Triguero Piñero <juantriguero@live.com>
2017-01-19 14:56:16 +0100

    Editar perfil (cambiar contraseña)

--- EntHub/main/urls.py
+++ EntHub/main/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout_then_login
+from django.contrib.auth.views import login, logout_then_login, password_change
 from django.contrib.auth.decorators import login_required
 from main import views
 
@@ -8,8 +8,10 @@ urlpatterns = [
     url(r'^accounts/login/', login, {'template_name': 'main/login.html'}, name='login'),
     url(r'^accounts/logout/', logout_then_login, name='logout'),
     url(r'^accounts/register/', views.UserRegister.as_view(), name='register'),
-    
+
     url(r'^accounts$', login_required(views.account_list), name='account_list'),
 	url(r'^accounts/(?P<pk>\d+)/', login_required(views.AccountDetail.as_view()), name='account_detail'),
     url(r'^accounts/edit/', login_required(views.AccountUpdate.as_view()), name='account_update'),
+    url(r'^accounts/password/', login_required(password_change), {'template_name': 'main/password_change.html',
+    		'post_change_redirect': 'main:account_update'}, name='password_change'),
 ]

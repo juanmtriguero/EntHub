874da5db3408144657be370a6d6f8076effaf176
Juan Manuel Triguero Piñero <juantriguero@live.com>
2017-03-02 16:21:09 +0100

    Test de prueba para index

--- EntHub/main/tests.py
+++ EntHub/main/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+# main.views.index
+class IndexTestCase(TestCase):
+
+	def setUp(self):
+		user = User.objects.create_user('user', 'user@mail.com', 'user')
+
+	# Unregistered users are redirected to login
+	def test_unregistered_index(self):
+		response = self.client.get('/', follow=True)
+		self.assertEqual(response.status_code, 302)
+		self.assertTemplateUsed(response, 'main/login.html')
+
+	# Registered users go to index
+	def test_unregistered_index(self):
+		self.client.login(username='user', password='user')
+		response = self.client.get('/')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'main/index.html')

aae16725f99957f5a158fc4732aa44ce2a971164
Juan Manuel Triguero Piñero <juantriguero@live.com>
2017-03-04 02:10:27 +0100

    Unificadas migraciones de Account_following

--- EntHub/main/migrations/0003_account_following.py
+++ EntHub/main/migrations/0003_account_following.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='account',
             name='following',
-            field=models.ManyToManyField(related_name='_account_following_+', to='main.Account', blank=True),
+            field=models.ManyToManyField(related_name='followers', to='main.Account', blank=True),
         ),
     ]

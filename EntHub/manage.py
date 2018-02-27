20a29366baba4427bb90d92e3eef83a6d5384c41
Juan Manuel Triguero Piñero <juantriguero@live.com>
2018-02-27 23:08:30 +0100

    Heroku usa sus propias settings

--- EntHub/manage.py
+++ EntHub/manage.py
@@ -3,7 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EntHub.settings")
+
+	# Specific settings for Heroku
+	if 'IS_HEROKU' in os.environ:
+		os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EntHub.heroku_settings")
+	else:
+    	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EntHub.settings")
 
     from django.core.management import execute_from_command_line
 
